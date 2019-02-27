1e3c1dfdf57c5c1579d7f5517738c83ae73344dd
hcool2489 <hcool2489@gmail.com>
2019-02-27 10:46:06 +0530

    init commit

---
+++ src/DoublyLinkedList.java
@@ -0,0 +1,28 @@
+public class DoublyLinkedList {
+    private static class Node{
+        private int data;
+        private Node pre;
+        private Node next;
+
+        public Node(int data, Node pre, Node next) {
+            this.data = data;
+            this.pre = pre;
+            this.next = next;
+        }
+
+        public Node(int data) {
+            this.data = data;
+            this.pre = null;
+            this.next = null;
+        }
+    }
+
+    private int size = 0;
+    private Node head = null;
+    private Node tail = null;
+
+    public static void main(String[] args) {
+        DoublyLinkedList linkedList = new DoublyLinkedList();
+        System.out.println(linkedList);
+    }
+}
